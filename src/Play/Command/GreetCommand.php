d91c6d1830497b706255d8f692d56a09f3bda44e
Toon Ketels <toon@puresign.be>
2012-06-02 13:56:03 +0200

    Options and arguments.

--- src/Play/Command/GreetCommand.php
+++ src/Play/Command/GreetCommand.php
@@ -14,23 +14,29 @@ class GreetCommand extends Command
   {
     $this->setName('demo:greet')
          ->setDescription('Greet someone')
-         ->addArgument('name', InputArgument::OPTIONAL, 'Who do you want to greet?')
-         ->addOption('yell', null, InputOption::VALUE_NONE, 'If set, the task will yell in uppercase letters.');
+         ->addArgument('name', InputArgument::REQUIRED, 'Who do you want to greet?')
+         ->addArgument('last_name', InputArgument::OPTIONAL, 'Your last name?')
+         ->addOption('yell', null, InputOption::VALUE_NONE, 'If set, the task will yell in uppercase letters.')
+         ->addOption('iterations', null, InputOption::VALUE_REQUIRED, 'How many times should the message be printed?', 1);
   }
 
   protected function execute(InputInterface $input, OutputInterface $output)
   {
     $name = $input->getArgument('name');
-    if($name) {
-      $text = 'Hello '.$name;
+    $last_name = $input->getArgument('last_name');
+
+    if($last_name) {
+      $text = sprintf('Hello %s %s', $name, $last_name);
     } else {
-      $text = 'Hello';
+      $text = sprintf('Hello %s', $name);
     }
 
     if($input->getOption('yell')) {
       $text = strtoupper($text);
     }
 
-    $output->writeln($text);
+    for($i = 0; $i < $input->getOption('iterations'); $i++) {
+      $output->writeln($text);
+    }
   }
 }
